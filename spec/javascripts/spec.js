96d971c495417f7bfb70c988ad81506081e0aeeb
heartsentwined <heartsentwined@cogito-lab.com>
2013-04-20 18:31:49 +0800

    spec.js no longer needed

--- spec/javascripts/spec.js
+++
@@ -1,9 +0,0 @@
-//= require jquery
-//= require underscore
-//= require handlebars
-//= require ember
-//= require ember-data
-//= require ../../lib/ember-auth
-//= require_tree ./helpers
-//= require_tree ./examples
-//= require_tree .
